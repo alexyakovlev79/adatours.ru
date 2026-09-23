c671135fc59a801b6458c56422ca19b89b0f0c5c
alexyakovlev79 <alex.yakovlev79@gmail.com>
2026-09-24 02:03:02 +0300

    Add verified contact facts and direct contact actions

---
+++ src/data/organization/ada-tours.ts
@@ -0,0 +1,12 @@
+export const ORG = {
+  brand: 'Ada Tours',
+  email: 'info@adatours.com',
+  phoneDisplay: '+55 21 3435-6408',
+  phoneHref: 'tel:+552134356408',
+  whatsappDisplay: '+55 21 98848-4456',
+  whatsappHref: 'https://wa.me/5521988484456',
+  address: 'Nsa Sra de Copacabana 680/916, Copacabana, Rio de Janeiro, Brazil',
+  telegramHref: null as string | null,
+  factStatus: 'review',
+  checkedAt: '2026-09-24',
+};
